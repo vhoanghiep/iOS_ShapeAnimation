24945b58f3ea8b288a38b6ac7e6f13a55e5aab87
hiepvh <vhoanghiep103@gmail.com>
2020-08-03 13:22:22 +0700

    Initial Commit

---
+++ ShapeAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShapeAnimation
+//
+//  Created by Hoang Hiep on 8/3/20.
+//  Copyright © 2020 Hoang Hiep. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
